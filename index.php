903cd6b87156cab5420af0c7f4dec800e96d4d7e
Vegadrone <spenaantonino@gmail.com>
2022-09-02 16:16:47 +0200

    Add the instances in movies array and print in page title, genre, director, year and rating using foreach

--- index.php
+++ index.php
@@ -1,22 +1,31 @@
-<?php 
-    require_once __DIR__ . ('/classes/movie.php');
+<?php
+require_once __DIR__ . ('/classes/movie.php');
 
-    $dracula = new Movie ('Dracula', 'Horror', 'Todd Browning', 1931, 7.4);
-    $nosferatu = new Movie ('Nosferatu', 'Horror', 'F.W. Murnau', 1922, 7.9);
+$movies = [];
+$movies[] = new Movie('Dracula', 'Horror', 'Todd Browning', 1931, 7.4);
+$movies[] = new Movie('Nosferatu', 'Horror', 'F.W. Murnau', 1922, 7.9);
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Movies</title>
+    <title>Movies - PHP OOP</title>
 </head>
+
 <body>
     <h1>MOVIES</h1>
     <ul>
-
+        <?php foreach ($movies as $movie) { ?>
+            <li>
+                <?php echo $movie->getTitle() . " - " . $movie->getGenre() . " - " . $movie->getDirector() . " - " . $movie->getYear() . " - " . $movie->getRating();  ?>
+            </li>
+        <?php } ?>
     </ul>
 </body>
+
 </html>
