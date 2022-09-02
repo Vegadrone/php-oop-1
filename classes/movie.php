984ec0ebad973a10e7f4a5dc84a481df0bd98350
Vegadrone <spenaantonino@gmail.com>
2022-09-02 15:25:09 +0200

    Create inedx.php, classes folder and movie.php. Create class Movie.

---
+++ classes/movie.php
@@ -0,0 +1,17 @@
+<?php 
+    class Movie {
+        
+        //properties
+        private $title;
+        private $genre;
+        private $director;
+        private $year;
+        private $rating;
+        
+        //constructor
+        public function __construct($_title, $_genre, $_director, $_year, $_rating){
+            
+        }
+
+    }
+?>
